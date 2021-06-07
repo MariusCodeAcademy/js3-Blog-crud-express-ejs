8ccc4c61616df6fb1af47f12965f582b4320de67
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-07 15:03:59 +0300

    uzd comm

---
+++ src/models/comment.js
@@ -0,0 +1,9 @@
+// 1 sukurti modeli su bent 2 iveties laukais
+
+// 2. susieti comment modeli su blog modeliu
+
+// 3. sukurti forma comment sukurimui
+
+// 4. sukurti keleta commentaru keletui blog postu
+
+// 5 isitikinti kad viskas veikia
