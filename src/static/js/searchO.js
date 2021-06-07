972b3b73f0035bbbc6a2c6c2751ac782d571f5d0
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-07 08:18:50 +0300

    added events on form and input

--- src/static/js/searchO.js
+++ src/static/js/searchO.js
@@ -1,1 +1,11 @@
 console.log('search Owner');
+
+const searchOFormEl = document.getElementById('search-o');
+const searchInputEl = searchOFormEl.querySelector('input');
+
+searchOFormEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('submit event');
+});
+
+searchInputEl.addEventListener('input', () => console.log('input event'));
